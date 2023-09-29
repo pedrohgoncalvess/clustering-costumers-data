fb1962cc99a1cd3da9cb393387ba337c13180fdf
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-29 12:26:35 -0300

    add: starting processing in the socios dataframe

---
+++ src/main/scala/datasets/socios_frame.scala
@@ -0,0 +1,12 @@
+package datasets
+
+class socios_frame {
+
+  val sociosColumns: Seq[String] = Seq(
+    "cnpj_basico", "identific_socio", "nome_socio",
+    "cnpj-cpf", "qualif_socio", "dt_entrada_socie",
+    "pais", "represent_legal", "nm_representant",
+    "qualif_represent_legal", "faixa_etaria"
+  )
+
+}
