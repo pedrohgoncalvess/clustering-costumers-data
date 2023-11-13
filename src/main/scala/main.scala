6634732f1f695f3167e416b4593859534fec0e56
unknown <pedro_gonsalves@hotmail.com>
2023-11-13 10:43:24 -0300

    add: export empresas frame treatment

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,12 +1,13 @@
-package datasets
-
+import datasets.EstabelecimentosFrame.estabelecimentosTreatment
+import datasets.EmpresasFrame.empresasTreatment
 import datasets.initSparkSession.write_parquet_files
-import datasets.projectConfigs.estabelecSubDir
-import estabelecimentos_frame.read_csv_estabelec_files
 
 object main extends App{
 
-  val dataframeEstabelec = read_csv_estabelec_files
-  write_parquet_files(dataframe=dataframeEstabelec, columnToPartition="uf", subDir=estabelecSubDir)
+  //val dataFrameEstabelec = estabelecimentosTreatment
+  //write_parquet_files(dataframe=dataFrameEstabelec, columnToPartition="uf", subDir="estabelecimentos")
+
+  val dataFrameEmpresas = empresasTreatment
+  write_parquet_files(dataframe=dataFrameEmpresas, columnToPartition="uf", subDir="empresas")
 
 }
