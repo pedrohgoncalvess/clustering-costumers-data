b9fb11629b827da29b03bab7a21eb9c28e1273a9
unknown <pedro_gonsalves@hotmail.com>
2023-11-13 10:42:44 -0300

    refac: create new package to centralize general spark variables

--- src/main/scala/sparkLocal/init_spark_local.scala
+++ src/main/scala/sparkLocal/init_spark_local.scala
@@ -1,6 +1,8 @@
 package datasets
 
+import environment.projectConfigs.{create_treated_datasets_dir, rootPath, treatedFilesPath}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql
 
 object initSparkSession {
 
@@ -9,5 +11,16 @@ object initSparkSession {
       .master("local[*]")
       .appName("SparkLocal")
       .getOrCreate()
+  }
+
+  def write_parquet_files(dataframe: sql.DataFrame, columnToPartition: String = "None", subDir: String): Unit = {
+    create_treated_datasets_dir
+    val treatedDir = s"$rootPath\\$treatedFilesPath\\$subDir"
+    if (columnToPartition == "None") {
+      dataframe.write.mode("overwrite").parquet(treatedDir)
+    }
+    else {
+      dataframe.write.mode("overwrite").partitionBy(columnToPartition).parquet(treatedDir)
+    }
   }
 }
